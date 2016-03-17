6f2a0dc457ab255934744a07395cfb2e73f0d219
Matthias Zimmermann <matthias.zimmermann@bsiag.com>
2016-03-17 16:25:54 +0100

    add TODO for Spring integration (and a new startup ascii art)

--- helloworld.server/src/main/java/org/eclipse/scout/apps/helloworld/server/spring/GreetingController.java
+++ helloworld.server/src/main/java/org/eclipse/scout/apps/helloworld/server/spring/GreetingController.java
@@ -2,20 +2,25 @@ package org.eclipse.scout.apps.helloworld.server.spring;
 
 import org.eclipse.scout.apps.helloworld.server.Counter;
 import org.eclipse.scout.apps.helloworld.server.model.Greeting;
-
 import org.eclipse.scout.rt.platform.BEANS;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * test for this rest service: http://localhost:8081/greeting?name=Alice
+ * expected result (the id value may differ): {"id":1,"content":"Hello, Alice!"} 
+ */
+// TODO [mzi]: make spring boot use the available servlet container (it currently starts the embedded tomcat)
+// without this rest service, it would be enough to say 'app.setWebEnvironment(false);' in the PlatformListener class
+// alternatively make the scout platform use the tomcat embedded by springboot
 @RestController
 public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-    	Long id = BEANS.get(Counter.class).nextValue();
-    	String content = SPRING.get(Hello.class).getText(name);
+    	Long id = BEANS.get(Counter.class).nextValue(); // use a scout bean
+    	String content = SPRING.get(Hello.class).getText(name); // use a spring component
         return new Greeting(id, content);
     }
 }
--- helloworld.server/src/main/java/org/eclipse/scout/apps/helloworld/server/spring/PlatformListener.java
+++ helloworld.server/src/main/java/org/eclipse/scout/apps/helloworld/server/spring/PlatformListener.java
@@ -7,19 +7,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class PlatformListener implements IPlatformListener {
+public class PlatformListener 
+	extends SpringBootServletInitializer 
+	implements IPlatformListener  
+{
 	private static final Logger LOG = LoggerFactory.getLogger(PlatformListener.class);
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PlatformListener.class);
+    }
+	
 	@Override
 	public void stateChanged(PlatformEvent event) {
 		if (event.getState() == State.BeanManagerValid) {
 			LOG.info("Scout platform ready");
-			
-			String [] args = new String[] {};
-			ApplicationContext ctx = SpringApplication.run(PlatformListener.class, args);
+
+			SpringApplication app = new SpringApplication(PlatformListener.class);
+			ApplicationContext ctx = app.run();
 			SPRING.setContext(ctx);
 			LOG.info("Spring platform ready");
 		}
