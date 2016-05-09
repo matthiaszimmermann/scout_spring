12e0627d26c4801a3b985e683519ccd409473ffe
Matthias Zimmermann <matthias.zimmermann@bsiag.com>
2016-05-09 23:52:59 +0200

    update for neon m7

--- helloworld.server/src/main/java/org/eclipse/scout/apps/helloworld/server/permission/AccessControlService.java
+++ helloworld.server/src/main/java/org/eclipse/scout/apps/helloworld/server/permission/AccessControlService.java
@@ -1,26 +1,23 @@
 package org.eclipse.scout.apps.helloworld.server.permission;
 
-import java.security.AllPermission;
-import java.security.Permissions;
+import java.security.PermissionCollection;
 
-import org.eclipse.scout.rt.platform.Order;
-import org.eclipse.scout.rt.shared.security.RemoteServiceAccessPermission;
-import org.eclipse.scout.rt.shared.services.common.security.UserIdAccessControlService;
+import org.eclipse.scout.rt.shared.services.common.security.AbstractAccessControlService;
 
 /**
  * <h3>{@link AccessControlService}</h3>
  *
  * @author mzi
  */
-@Order(-1)
-public class AccessControlService extends UserIdAccessControlService {
-	@Override
-	protected Permissions execLoadPermissions(String userId) {
-		Permissions permissions = new Permissions();
-		permissions.add(new RemoteServiceAccessPermission("*.shared.*", "*"));
+public class AccessControlService extends AbstractAccessControlService<String> {
 
-		// TODO [mzi]: Fill access control service
-		permissions.add(new AllPermission());
-		return permissions;
+	  @Override
+	  protected String getCurrentUserCacheKey() {
+	    return getUserIdOfCurrentUser();
+	  }
+
+	  @Override
+	  protected PermissionCollection execLoadPermissions(String userId) {
+	    return null;
+	  }
 	}
-}
